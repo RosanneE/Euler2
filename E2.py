de05c7f6962f19894ef0f8848d6b67d65a6f05bf
RosanneE <anderson.rosanne4@gmail.com>
2015-06-22 16:49:11 -0400

    Create E2.py

---
+++ E2.py
@@ -0,0 +1,13 @@
+// print even fib sequence
+
+int fib = 1
+int bucketA = 1
+int bucketB = 0
+int total = 0
+
+while (fib < 4,000,000)
+  bucketB = bucketA
+  fib = fib + bucketA
+  bucketA
+  curr = 
+
